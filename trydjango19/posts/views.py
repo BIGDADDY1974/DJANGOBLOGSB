5b8cf495d1cf25ad7921b0b260b3b19eadf822b2
BIGDADDY1974 <svilar@beotel.rs>
2016-02-15 13:40:42 +0100

    adding new stuff to the repo, the views and urls are sorted mow

--- trydjango19/posts/views.py
+++ trydjango19/posts/views.py
@@ -5,6 +5,21 @@ from django.http import HttpResponse
 def startup(request):
     return HttpResponse("<h1>THIS IS THE STARUP PAGE</h1>"
                         "<a href='http://127.0.0.1:8000/posts/'>Go to POSTS</a>")
-
 def post_home(request):
     return HttpResponse("<h1>HELLO DJANGO BANGO</h1>")
+
+def post_create(request):
+    return HttpResponse("<h1>HELLO CREATE</h1>")
+
+def post_detail(request):
+    return HttpResponse("<h1>HELLO DETAIL</h1>")
+
+def post_list(request):
+    return HttpResponse("<h1>HELLO LIST</h1>")
+
+def post_update(request):
+    return HttpResponse("<h1>HELLO UPDATE</h1>")
+
+def post_delete(request):
+    return HttpResponse("<h1>HELLO DELETE</h1>")
+
