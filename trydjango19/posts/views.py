25a80728996a1bb8886be0db1655a4913e52808b
BIGDADDY1974 <svilar@beotel.rs>
2016-02-15 14:24:48 +0100

    adding new stuff to the repo, whetather the user is authenticated or not change the htmpl and view lists

--- trydjango19/posts/views.py
+++ trydjango19/posts/views.py
@@ -6,16 +6,33 @@ def startup(request):
     return HttpResponse("<h1>THIS IS THE STARUP PAGE</h1>"
                         "<a href='http://127.0.0.1:8000/posts/'>Go to POSTS</a>")
 def post_home(request):
-    return HttpResponse("<h1>HELLO DJANGO BANGO</h1>")
-
-def post_create(request):
-    return HttpResponse("<h1>HELLO CREATE</h1>")
-
-def post_detail(request):
-    return HttpResponse("<h1>HELLO DETAIL</h1>")
+    context = {
+       "title":"HOME"
+    }
+    return render(request, 'index.html', context)
 
 def post_list(request):
-    return render(request, 'index.html', {})
+    if request.user.is_authenticated():
+        context = {
+       "title":"USER AUTHENTICATED"
+    }
+    else:
+        context = {
+       "title":"USER NOT AUTHENTICATED"
+    }
+    return render(request, 'index.html', context)
+
+def post_create(request):
+    context = {
+       "title":"Create"
+    }
+    return render(request, 'index.html', context)
+
+def post_detail(request):
+    context = {
+        "title":"Detail"
+    }
+    return render(request, 'index.html', context)
 
 def post_update(request):
     return HttpResponse("<h1>HELLO UPDATE</h1>")
